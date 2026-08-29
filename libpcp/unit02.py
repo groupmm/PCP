c42ce588cf09ba75a81e40515f8ec72989eb0b8d
Meinard Müller <meinard@alabsad.fau.de>
2026-08-29 12:22:37 +0200

    Unit 02, Unit 03

--- libpcp/unit02.py
+++ libpcp/unit02.py
@@ -14,28 +14,31 @@ def exercise_list(show_result=True):
     Notebook: PCP_02_python.ipynb
 
     Args:
-        show_result: Show result (Default value = True)
+        show_result: Show the sample solution if True.
     """
-    if show_result is False:
+    if not show_result:
         return
 
-    student_list = [[123, 'Meier', 'Sebastian'], [456, 'Smith', 'Walter']]
-    print(student_list)
+    student_list = [
+        [123, 'Meier', 'Max'],
+        [456, 'Smith', 'Sam']
+    ]
+    print('Initial:', student_list)
 
-    student_list = student_list + [[789, 'Wang', 'Ming']]
-    print(student_list)
+    student_list.append([789, 'Wang', 'Wei'])
+    print('Extended:', student_list)
 
-    print(student_list[::-1])
-
-    print(student_list[1][2])
-
-    print(len(student_list))
+    print('Reversed:', student_list[::-1])
+    print('First name of second student:', student_list[1][2])
+    print('Number of students:', len(student_list))
 
     student_list_copy = copy.deepcopy(student_list)
-    del(student_list_copy[0:2])
+    del student_list_copy[:2]
     student_list_copy[0][0] = 777
-    print(student_list_copy)
-    print(student_list)
+
+    print('Modified copy:', student_list_copy)
+    print('Original:', student_list)
+    print('Original ID remains unchanged:', student_list[-1][0] == 789)
 
 
 def exercise_dict(show_result=True):
@@ -44,24 +47,25 @@ def exercise_dict(show_result=True):
     Notebook: PCP_02_python.ipynb
 
     Args:
-        show_result: Show result (Default value = True)
+        show_result: Show the sample solution if True.
     """
-    if show_result is False:
+    if not show_result:
         return
 
-    student_dict = {123: ['Meier', 'Sebastian'], 456: ['Smith', 'Walter']}
-    print(student_dict)
+    student_dict = {
+        123: ['Meier', 'Max'],
+        456: ['Smith', 'Sam']
+    }
+    print('Initial:', student_dict)
 
-    student_dict[789] = ['Wang', 'Ming']
-    print(student_dict)
+    student_dict[789] = ['Wang', 'Wei']
+    print('Extended:', student_dict)
 
-    print(list(student_dict.keys()))
-
-    print(list(student_dict.values()))
-
-    print(student_dict[456][0])
+    print('Student IDs:', list(student_dict.keys()))
+    print('Student names:', list(student_dict.values()))
+    print('Last name for ID 456:', student_dict[456][0])
 
     del student_dict[456]
-    print(student_dict)
+    print('After deleting ID 456:', student_dict)
 
-    print(len(student_dict))
+    print('Number of remaining students:', len(student_dict))
